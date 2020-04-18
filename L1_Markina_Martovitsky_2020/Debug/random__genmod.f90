657d8b039f236cdca258b5d970b0c9b3ebce1c33
Alevtina Markina <43007278+markinaalya@users.noreply.github.com>
2020-04-18 20:49:38 +0300

    vol 1
    проблема с if

---
+++ L1_Markina_Martovitsky_2020/Debug/random__genmod.f90
@@ -0,0 +1,12 @@
+        !COMPILER-GENERATED INTERFACE MODULE: Sat Apr 18 20:04:34 2020
+        ! This source file is for reference only and may not completely
+        ! represent the generated interface used by the compiler.
+        MODULE RANDOM__genmod
+          INTERFACE 
+            FUNCTION RANDOM(A,B)
+              REAL(KIND=4), INTENT(IN) :: A
+              REAL(KIND=4), INTENT(IN) :: B
+              REAL(KIND=4) :: RANDOM
+            END FUNCTION RANDOM
+          END INTERFACE 
+        END MODULE RANDOM__genmod
